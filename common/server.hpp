e6ff80b7ca0636f5de89333d70f3eb701f237958
ghostware0x00 <souravbose361@gmail.com>
2026-08-05 05:29:37 -0400

    send commands function prototype update

--- common/server.hpp
+++ common/server.hpp
@@ -3,7 +3,8 @@
 class server{
     public:
         void listener();
-        void send_commands(int soc_fd);
+        void send_commands(int soc_fd, int session_id);
+        int get_session_id();
 };
 
 //server common code header file
