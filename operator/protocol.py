13f91301ea9d0f8cdb0c1fe42eb4775e1c91568c
ghostware0x00 <souravbose361@gmail.com>
2026-08-16 13:29:34 -0400

    coding the packet structure for operator

--- operator/protocol.py
+++ operator/protocol.py
@@ -1,2 +1,7 @@
 import struct
 
+# we are sending data in bytes in network
+# in this protocol.py we are arranging what bytes are in what position
+# or in what order based on which we can read them in recv()
+# and run the respective function or operation
+
